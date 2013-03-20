91cc9cbb4b83ddd4877a77f3357f441020df224b
maxbiag80 <m.biagioli@live.it>
2013-03-20 14:38:37 +0100

    Implementato servizio di test con parametro

--- raspybox/services.py
+++ raspybox/services.py
@@ -4,10 +4,10 @@ Web Services
 from app import app
 
 
-@app.route("/test/")
-def test():
+@app.route("/test/<signal>")
+def test(signal):
     '''
     Servizio di test
     Utilizzato per testare il corretto funzionamento dell'applicativo
     '''
-    return "Test Service OK!"
+    return "Test Service OK! Signal=%s" % signal
